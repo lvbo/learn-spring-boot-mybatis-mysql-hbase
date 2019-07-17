b42ca8f1baf86d3cdd5c197a41abf9686e077dbc
lvbo <lvbo09@gmail.com>
2019-07-17 18:26:55 +0800

    试一下flyway是否支持Phoenix，不支持

---
+++ src/main/java/io/github/lvbo/learn/spring/boot/mmh/config/FlywayConfig.java
@@ -0,0 +1,32 @@
+package io.github.lvbo.learn.spring.boot.mmh.config;
+
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.configuration.FluentConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+/**
+ * @author lvbo
+ * @version V1.0
+ * @date 2019-07-17 10:43
+ */
+//@Configuration
+public class FlywayConfig {
+
+    @Autowired
+    @Qualifier("mysqlDataSource")
+    private DataSource mysqlDataSource;
+
+    @PostConstruct
+    public void migrateFlyway() {
+        FluentConfiguration mysqlFluentConfiguration = new FluentConfiguration();
+        mysqlFluentConfiguration.dataSource(mysqlDataSource);
+        mysqlFluentConfiguration.cleanDisabled(true);
+        mysqlFluentConfiguration.locations("db/migration");
+        Flyway mysqlFlyway = new Flyway(mysqlFluentConfiguration);
+        mysqlFlyway.migrate();
+    }
+}
---
+++ src/main/resources/db/migration/V1.0.1__create-user.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `user_name` varchar(32) DEFAULT NULL COMMENT '用户名',
+  `pass_word` varchar(32) DEFAULT NULL COMMENT '密码',
+  `user_sex` varchar(32) DEFAULT NULL,
+  `nick_name` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
